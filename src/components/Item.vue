826e149ade7cca0a811964dc7425c68b065009e4
nathan <r4sl3xgod@gmail.com>
2024-02-20 02:34:10 +0700

    Буду щас делать кнопоньки

---
+++ src/components/Item.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+  <a href="#" class="item">
+    <slot></slot>
+  </a>
+</template>
+
+<style scoped>
+.item{
+  color: black;
+  background: #ECEEF6;
+  border-radius: 10px 10px 0 0;
+  padding: 18px 30px;
+}
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,28 @@
+<script setup>
+import item from "@/components/Item.vue";
+</script>
+
+<template>
+  <div class="nav-block">
+    <nav class="nav-list">
+      <item @click="$router.push('/')">Каталог</item>
+      <item @click="$router.push('/order')">Оформленные товары</item>
+      <item @click="$router.push('/basket')">Корзина</item>
+    </nav>
+    <nav class="nav-list">
+      <item @click="$router.push('/authorization')">Вход</item>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+.nav-block{
+  display: flex;
+  justify-content: space-between;
+}
+
+.nav-list{
+  display: flex;
+  gap: 6px;
+}
+</style>
