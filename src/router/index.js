ed765d15f63c54cf266632bcd053f0649d32169f
nathan <r4sl3xgod@gmail.com>
2024-02-20 02:12:37 +0700

    Пишу маршрутки

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
-  }
-]
+import CatalogPage from "@/pages/CatalogPage.vue";
+import OrderPage from "@/pages/OrderPage.vue";
+import BasketPage from "@/pages/BasketPage.vue";
+import LoginPage from "@/pages/LoginPage.vue";
+import RegistrationPage from "@/pages/RegistrationPage.vue";
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'catalog',
+      component: CatalogPage
+    },
+    {
+      path: '/order',
+      name: 'order',
+      component: OrderPage
+    },
+    {
+      path: '/basket',
+      name: 'basket',
+      component: BasketPage
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginPage
+    },
+    {
+      path: '/registration',
+      name: 'registration',
+      component: RegistrationPage
+    },
+  ]
 })
 
 export default router
